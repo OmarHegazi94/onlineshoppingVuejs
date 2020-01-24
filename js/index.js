c9cf33ef5efaece15014d9b47855acf79d4bce83
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2020-01-24 10:16:22 +0200

    added items to cart + if exists will increase quantity without adding another row in the cart

--- js/index.js
+++ js/index.js
@@ -1,10 +1,57 @@
+
 vm = new Vue({
-    el: "#main",
+    el: "main",
     data: {
-        products: products
+        products: products,
+        cart: {
+            items: [],
+        },
+        cardActive: false,
     },
     filters: {
-        // currency: 
+        currency: function(value) {
+            let formatter = Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 0
+            });
+
+            return formatter.format(value);
+        }
+    },
+    methods: {
+        // check if the product already is in the cart
+        // to not draw another table row for the same product
+        checkexistproduct(product) {
+            testProduct = null;
+
+            for(let i =0; i<this.cart.items.length; i++){
+                if(this.cart.items[i].product.id === product.id) {
+                    testProduct = this.cart.items[i].product;
+                    break;
+                }
+            }
+            return testProduct;
+        },
+        addTocart: function(product){
+            // call to checkexistproduct(product)
+            if(this.checkexistproduct(product) == null) {
+                this.cart.items.push({
+                    product:product,
+                    quantity: 1
+                });
+
+            } else {
+                // product exists in the cart
+                for(let x =0; x < this.cart.items.length; x++) {
+                    if(this.cart.items[x].product.id == product.id){
+                        this.cart.items[x].quantity++;
+                        break;
+                    }
+                }
+            }
+            product.instock--;
+        }
     },
     computed: {
 
---
+++ js/products.js
@@ -0,0 +1,53 @@
+products=[
+    {
+        id:100,
+        name:'product One',
+        price:1000,
+        instock:5,
+        description:"product One is standard it to make a type specimen book. leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://placekitten.com/250/250'
+    },
+    {
+        id:200,
+        name:'product Two',
+        price:2000,
+        instock:15,
+        description:"product Two is standard it to make a type specimen book. leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://www.placecage.com/250/250'
+    },
+    {
+        id:300,
+        name:'product Three',
+        price:3000,
+        instock:500,
+        description:"product Three is standard it to make a type specimen book. leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://picsum.photos/250/250'
+    },
+    {
+        id:400,
+        name:'product Four',
+        price:4000,
+        instock:800,
+        description:"product Four is standard it to make a type specimen book. leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250'
+    },
+    {
+        id:500,
+        name:'product Five',
+        price:5000,
+        instock:0,
+        description:"product Five is standard it to make a type specimen book. leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250'
+    },
+    {
+        id:600,
+        name:'product Six',
+        price:6000,
+        instock:3,
+        description:"product Six is standard it to make a type specimen book. leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250'
+    }
+
+
+
+];
